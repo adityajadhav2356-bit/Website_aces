c885c68795a1af20133be7b5d783093e79f3f5d4
Aaditya Jadhav <adityajadhav2356@gmail.com>
2026-03-15 18:21:08 +0530

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>ACES - Association of Computer Engineering Students</h1>
+      <p>Welcome to the official ACES website.</p>
+    </div>
+  );
+}
+
+export default App;
